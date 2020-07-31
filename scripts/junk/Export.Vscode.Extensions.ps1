1d38f42d25cabe1950dda97626848776d952f8e0
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2020-07-31 00:35:55 -0700

    Cleanup

--- scripts/junk/Export.Vscode.Extensions.ps1
+++
@@ -1,8 +0,0 @@
-[CmdletBinding()]
-param (
-    [string]$vscode_config_filepath = $(Join-Path (Split-Path $PSCommandPath) 'vscode.extensions.conf')
-)
-
-$ErrorActionPreference = 'Stop'
-
-code --list-extensions | Out-File $vscode_config_filepath ascii -Force
