84ccf2842d45d7d33565a62e16ff42744ae96062
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2020-08-02 22:36:29 -0700

    1.Moved error logging into Quit function; 2.Removed function: 'load_main_config'; 3.Added function 'stage_manager'; 4.Added function 'load_modules', moved modules import into that function

--- setup_dev_env.ps1
+++ setup_dev_env.ps1
@@ -1,59 +1,111 @@
 [CmdletBinding()]
 param (
-    [string]$config_file_path
+    [string]$config_file_path,
+    [string]$log_file_path
 )
 
 
 
-########## FUNCTIONS
-function Quit ($exit_code = 0) {
+########## FUNCTIONS ####################
+function Quit ($exit_code = 0, $exception) {
+    if ($exit_code -ne 0) {
+        Write-Log -Level ERROR -Message $exception.Exception.Message
+        Write-Log -Level DEBUG -Message $exception.InvocationInfo.PositionMessage
+    }
     Write-Log '==> END OF LOG <=='
-    $logpath = (Get-LoggingTarget File).Path
+    $log_file_path = (Get-LoggingTarget File).Path
     Wait-Logging
     Remove-Module Logging -Force
-    Write-Host "Log path: '$logpath'" -ForegroundColor Yellow
-    if ($exit_code -ne 0) { notepad $logpath }
+    Write-Host "Log path: '$log_file_path'" -ForegroundColor Yellow
+    if ($exit_code -ne 0) { notepad $log_file_path }
     exit $exit_code
 }
 
-function load_main_config {
+function stage_manager {
     param (
-        [string]$config_file_path = 'config.yml'
+        [Parameter(ParameterSetName = "init")]
+        [switch]$init,
+
+        # [Parameter(Mandatory = $true, ParameterSetName = "init")]
+        # [string]$log_file_path,
+
+        [Parameter(ParameterSetName = "config")]
+        $config,
+
+        [Parameter(ParameterSetName = "config")]
+        [string]$config_file_path
     )
 
-    Write-Log "Installing module: powershell-yaml"
-    if (!(Get-Module powershell-yaml)) {
-        if (!(Get-Module powershell-yaml -ListAvailable)) {
-            Install-Module powershell-yaml -Force -Scope CurrentUser
+    $stage_control_filepath = Join-Path $PSScriptRoot ".$($script:main_script_basename).stage"
+    $stage_control_file_exists = Test-Path $stage_control_filepath
+    $stages = @()
+
+    if ($init) {
+        try {
+            if ($stage_control_file_exists) {
+                $log_file_path = (cat $stage_control_filepath -Raw | ConvertFrom-Yaml -Ordered).$log_file_path
+                return $log_file_path
+            }
+            else {
+                ni $stage_control_filepath -ItemType File -Force | Out-Null
+                Write-Log '<== BEGINNING OF LOG ==>'
+                return (Get-LoggingTarget File).Path
+            }
         }
-        Import-Module powershell-yaml -DisableNameChecking
+        catch {
+            Quit 1 $_
+        }
     }
 
-    Write-Log "Loading configuration from '$config_file_path'"
-    $config = cat $config_file_path -Raw | ConvertFrom-Yaml -Ordered
 
-    return $config
+
+    if ($config) {
+        # foreach ($s) {}
+    }
+    #(ps)
+    # if ($stage -ne 'vars') {
+
+    # }
+    #return $stages
+}
+
+function load_modules ([string[]]$modules) {
+    foreach ($module_name in $modules) {
+        Write-Log "Installing module: $module_name"
+        if (!(Get-Module $module_name)) {
+            if (!(Get-Module $module_name -ListAvailable)) {
+                Install-Module $module_name -Force -Scope CurrentUser
+            }
+            Import-Module $module_name -DisableNameChecking
+        }
+    }
 }
 
 
 
-########## MAIN
+########## MAIN ####################
 $ErrorActionPreference = 'stop'
-$script:start_time = Get-Date
+$timestamp = get-date -f 'yyyy-MM-ddTHH-mm-ss'
+$script:main_script_basename = (gi $PSCommandPath).BaseName
 Import-Module .\scripts\util_functions.psm1 -Force -DisableNameChecking
 restart_elevated -script_args $PSBoundParameters
 
 
 ### Init Logger
 Write-Host "initializing logger" -ForegroundColor DarkGray
-$log_file_name = (get-date $script:start_time -f 'yyyy-MM-ddTHH-mm-ss'), ((gi $PSCommandPath).BaseName + '.log') -join ('_')
-$log_file_path = Join-Path (mkdir (Join-Path $PSScriptRoot '.logs') -Force).FullName $log_file_name
+if (!$log_file_path) {
+    $log_file_name = $timestamp, ($main_script_basename + '.log') -join ('_')
+    $log_file_path = Join-Path (mkdir (Join-Path $PWD.path '.logs') -Force).FullName $log_file_name
+}
+$log_file_path = $log_file_path | abspath
+
 if (!(Get-Module Logging)) {
     if (!(Get-Module Logging -ListAvailable)) {
         Install-Module Logging -Force -Scope CurrentUser
     }
     Import-Module Logging -DisableNameChecking
 }
+
 Add-LoggingTarget -Name Console -Configuration @{
     Level        = 'INFO'
     Format       = ' %{level}: %{message}'
@@ -64,34 +116,41 @@ Add-LoggingTarget -Name File -Configuration @{
     Format = '[%{timestamp}] [%{filename:15}, ln.%{lineno:-3}] [%{level:7}] %{message}'
     Path   = $log_file_path
 }
-Write-Log -Level info 'Logger is up'
+
+
+### Load modules
+load_modules powershell-yaml
+
+
+### Init stage manager
+$log_file_path = stage_manager -init
 
 
 ### Load main config
-if ($config_file_path) {
-    $config_file_path = $config_file_path | abspath
+if (!$config_file_path) {
+    $config_file_path = Join-Path $PSScriptRoot "$main_script_basename`.yml"
 }
-else {
-    $script_base_name = (gi $PSCommandPath).BaseName
-    $config_file_path = Join-Path $PSScriptRoot "$script_base_name`.yml"
-}
+$config_file_path = $config_file_path | abspath
 
 try {
-    $MAIN_CONFIG = load_main_config -config_file_path:$config_file_path
+    Write-Log "Loading configuration from '$config_file_path'"
+    $MAIN_CONFIG = cat $config_file_path -Raw | ConvertFrom-Yaml -Ordered
 }
 catch {
-    Write-Log -Level ERROR -Message $_.Exception.Message
-    Write-Log -Level DEBUG -Message $_.InvocationInfo.PositionMessage
-    Quit 1
+    Quit 1 $_
 }
 
 
 ### Execute scripts
-# TODO needs error handling
-foreach ($stage in $MAIN_CONFIG.keys) {
-    if ($stage -ne 'vars') {
+$stages = stage_manager -config $MAIN_CONFIG -config_file_path $config_file_path
+
+try {
+    foreach ($stage in $stages) {
         . (Resolve-Path $MAIN_CONFIG.$stage.script).Path -CONFIG $MAIN_CONFIG.$stage.config
     }
+}
+catch {
+    Quit 1 $_
 }
 
 
