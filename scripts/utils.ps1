97ee5d4d0f272d37b956c911cead9558ed643c48
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2020-09-13 23:51:22 -0700

    Addeed fix for vscode issue 'You have an older version of PackageManagement known to cause issues with PowerShell extension'

--- scripts/utils.ps1
+++ scripts/utils.ps1
@@ -5,29 +5,35 @@ param (
 
 $ErrorActionPreference = 'stop'
 
-foreach ($item in $CONFIG.sys_utils) {
-    Wait-Logging
-    Write-Log "- $($item.description)"
-    
-    $command = "& `"$($item.exe)`""
-
-    if ($item.args) {
-        $command += " $($item.args)"
+foreach ($util_type in $CONFIG.GetEnumerator()) {
+    if ($util_type.Key -eq 'rootdir') {
+        continue
     }
-    
-    $scriptblock = [scriptblock]::Create($command)
 
-    $LASTEXITCODE = 0
-    $success_exit_codes = $item.success_exit_codes
+    foreach ($item in $util_type.value) {
+        Wait-Logging
+        Write-Log "- $($item.description) ($($util_type.Key)"
 
-    if (!$success_exit_codes) {
-        $success_exit_codes = 0
-    }
-    
-    $output = icm $scriptblock
+        $command = "& `"$($item.exe)`""
 
-    if ($LASTEXITCODE -notin ([string]$success_exit_codes).split(',')) {
-        Write-Log -Level WARNING -Message $output
-        throw "Command FAILED: '$command' (exit code: $LASTEXITCODE)"
+        if ($item.args) {
+            $command += " $($item.args)"
+        }
+
+        $scriptblock = [scriptblock]::Create($command)
+
+        $LASTEXITCODE = 0
+        $success_exit_codes = $item.success_exit_codes
+
+        if (!$success_exit_codes) {
+            $success_exit_codes = 0
+        }
+
+        $output = icm $scriptblock
+
+        if ($LASTEXITCODE -notin ([string]$success_exit_codes).split(',')) {
+            Write-Log -Level WARNING -Message $output
+            throw "Command FAILED: '$command' (exit code: $LASTEXITCODE)"
+        }
     }
 }
