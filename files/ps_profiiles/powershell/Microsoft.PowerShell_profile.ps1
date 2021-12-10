f1c3e69b362c7274c3e3786f65581c5c4d837b63
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-12-09 23:23:04 -0600

    Reviewed and cleaned up

--- files/ps_profiiles/powershell/Microsoft.PowerShell_profile.ps1
+++ files/ps_profiiles/powershell/Microsoft.PowerShell_profile.ps1
@@ -4,100 +4,73 @@ if (Test-Path($ChocolateyProfile)) {
     Import-Module "$ChocolateyProfile"
 }
 
-# EnvVars >>
-# EnvVars <<
 
-# Functions >>
+# ENVIRONMENT VARIABLES
+# $env:PSModulePath = $env:PSModulePath.TrimEnd(';') + ';<additional_module_path>'
+# $env:Path = $env:Path.trim(';') + ';<some_path>'
+
+
+
+# FUNCTIONS
 function prompt {
     $current_location = $pwd.Path
-    (Get-Host).UI.RawUI.WindowTitle = (Get-Host).UI.RawUI.WindowTitle -replace '[C-Z]\:\\.*$', $current_location
-
-    $prompt = "@ "
-    $pathArray = $current_location.split('\')
-    if ($pathArray.length -lt 4) {
-        if ($pathArray[1].length -eq 0) {
-            $prompt += $current_location + ">"
-        }
-        else {
-            $prompt += $current_location + "\>"
-        }
-    }
-    else {
-        $prompt += $pathArray[0] + "\..\" + $pathArray[$pathArray.length - 2] + "\" + $pathArray[$pathArray.length - 1] + "\>"
-    }
-
+    (Get-Host).UI.RawUI.WindowTitle = $title.replace('<path>', $current_location)
+    $prompt = "`n$(split-path $current_location -leaf)/> "
     $prompt
 }
-function Invoke-ComputerSleep {
-    Add-Type -AssemblyName System.Windows.Forms
-    [System.Windows.Forms.Application]::SetSuspendState("Suspend", $false, $true);
-}
-function Test-RestartRequired {
-    $isRestartRequired = $false
 
-    if (Get-ChildItem "HKLM:\Software\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending" -EA Ignore) { $isRestartRequired = $true }
-    if (Get-Item "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\WindowsUpdate\Auto Update\RebootRequired" -EA Ignore) { $isRestartRequired = $true }
-    if (Get-ItemProperty "HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager" -Name PendingFileRenameOperations -EA Ignore) { $isRestartRequired = $true }
 
-    try { 
-        $util = [wmiclass]"\\.\root\ccm\clientsdk:CCM_ClientUtilities"
-        $status = $util.DetermineIfRebootPending()
-        if (($status -ne $null) -and $status.RebootPending) {
-            $isRestartRequired = $true
-        }
-    }
-    catch { }
+# MODULES
+$modules_to_install = @('UtilityFunctions')
+$modules_to_install | % { Import-Module $_ -Force -DisableNameChecking }
 
-    return $isRestartRequired
-}
-function Wait-AnyKey {
-    $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown") | Out-Null
-}
-# Functions <<
 
-# Modules >>
-# $env:PSModulePath = $env:PSModulePath.TrimEnd(';') + ';' + 'E:\Additional_Module_path'
-# Import-Module UtilFunctions -Force -DisableNameChecking
-# Modules <<
+# DEFAULTS
+$host.PrivateData.ErrorBackgroundColor = $host.UI.RawUI.BackgroundColor
+$PSDefaultParameterValues['*:Encoding'] = 'utf8'
+$default_locations = @(
+    'C:\Windows\system32',
+    'C:\Windows\System32\WindowsPowerShell\v1.0',
+    'C:\Program Files\PowerShell\7',
+    $HOME)
+$working_directory = 'E:\WORKSHOP'
 
-# Assemblies >>
-# Assemblies <<
 
-# Title >>
+# TITLE
 $title = whoami
 if (([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) { $title += "  ADMIN" }
 else { $title += "  (no admin)" }
-$title += "   $($pwd.path)"
+$title += "   <path>"
 (Get-Host).UI.RawUI.WindowTitle = $title
-# Title <<
 
-# Aliases >>
-Set-Alias -Name ics -Value Invoke-ComputerSleep
-Set-Alias -Name hib -Value Invoke-ComputerSleep
-Set-Alias -Name isres -Value Test-RestartRequired
-Set-Alias -Name wait -Value Wait-AnyKey
+
+# HEADER
+write "STARTED ON:`t$(get-date -UFormat %c)`n"
+
+
+# SET ALIASES
 Set-Alias -Name npp -Value "C:\Program Files\Notepad++\notepad++.exe"
-# Aliases <<
 
-# Commands >>
-$host.PrivateData.ErrorBackgroundColor = $host.UI.RawUI.BackgroundColor
-$PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
-if ($pwd.Path -eq 'C:\Windows\system32') {
-    try {
-        cd E:\GoogleDrive\stse.pavell\UsefullScripts -ErrorAction Stop
-    }
-    catch {
-        Write-Warning 'Directory not found: "E:\GoogleDrive\stse.pavell\UsefullScripts"'
-    }
+# PRINT ALIASES
+write "`nALIASES:"
+$amap = @{
+    'npp' = (Get-Alias npp).Definition
+}
+$amap.keys | sort | %{ write (" $_" + " "*(25 - $_.length) + "--> $($amap.$_)")}
+
+
+if (Get-Module UtilityFunctions) {
+    write "`nUTILITY FUNCTIONS:"
+    listmc UtilityFunctions
 }
 
-write-host "`nAliases:`n"
-(Get-Alias hib, isres, wait, npp).DisplayName | % { " $_" }
 
-Write-Host "`nCurrent directory: $($pwd.path)`n"
-if ($pwd.Path -ne 'C:\Windows\system32') {    
-    ls | % { " " + ($_.mode, $_.name -join ("`t")) }
-    write-host ""
+# WORKING DIRECTORY
+if ($pwd.Path -in $default_locations) {
+    try { cd $working_directory -ErrorAction Stop }
+    catch { Write-Warning "Directory not found: `"$working_directory`"" }
 }
-# Commands <<
+
+# FOOTER
+(([string][char]9552))*($Host.UI.RawUI.WindowSize.Width)
