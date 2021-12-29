16c7c061ecb04d12c6802bae9fa569ec8c4b8c34
Pavel Stsefanovich <pavel.stsefanovich@gmail.com>
2021-12-28 22:26:49 -0600

    Added script "packages.ps1" and config for it

--- scripts/packages.ps1
+++ scripts/packages.ps1
@@ -1,95 +1,103 @@
+[cmdletbinding()]
 param (
-    $CONFIG
+    [string]$ConfigFilePath = $(throw "Required parameter not provided: -ConfigFilePath"),
+    [string]$MainScriptDir = $(throw "Required parameter not provided: -MainScriptDir"),
+    [switch]$UpdateConfigInPlace
 )
 
 
 $ErrorActionPreference = 'stop'
+$excode = 1
+$IS_VERBOSE = [bool]($PSCmdlet.MyInvocation.BoundParameters.Verbose)
+$stage = (gi $PSCommandPath).BaseName
+$ConfigFilePath = $ConfigFilePath | abspath -verify
+$MainScriptDir = $MainScriptDir | abspath -verify
 
-Write-Log "Installing Chocolatey Package manager"
-Set-ExecutionPolicy Bypass -Scope Process -Force
-[System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
-iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+# dependencies
+if ( !(Get-Module UtilityFunctions) ) { Import-Module -Name powershell-yaml -Force -Scope Local -DisableNameChecking }
+if ( !(Get-Module powershell-yaml) ) { Import-Module -Name powershell-yaml -Force -Scope Local -DisableNameChecking }
 
+# load config
+$config = cat $ConfigFilePath -Raw | ConvertFrom-Yaml -Ordered
+$index = 0
 
-# install packages
-foreach ($package_type in $CONFIG.GetEnumerator()) {
-    if ($package_type.Key -eq 'rootdir') {
-        continue
+# deploy packages
+while ( $config.$stage[$index] ) {
+    $item = $config.$stage[$index]
+    info $item.description -sub
+    $failure = $null
+
+    $installer_filepath = $item.installer | abspath -parent $MainScriptDir
+
+    if ( ! (Test-Path $installer_filepath) ) {
+        # download installer
+        if ( $item.url ) {        
+            try {
+                [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12
+                $wc = New-Object System.Net.WebClient
+                $wc.DownloadFile($item.url, $installer_filepath)
+            }
+            catch {
+                $failure = "Donwload failed: " + $_.exception.InnerException.InnerException.Message
+            }
+        }
+        # fail if not found
+        else {
+            $failure = "Cannot find path `"$installer_filepath`""
+        }
     }
 
-    foreach ($item in $package_type.value) {
-        Wait-Logging
-        Write-Log "- installing '$($item.name)' ($($package_type.Key))"
+    # run installer
+    if ( ! $failure ) {
+        $success_exit_codes = $item.success_exit_codes
+        if ( ! $success_exit_codes ) { $success_exit_codes = @(0) }
 
-        # local installers
-        if ($package_type.Key -eq 'local') {
-            $executable_path = (Resolve-Path (Join-Path $CONFIG.rootdir $item.exe)).Path
-            $command = "& `"$executable_path`""
+        # type: msi
+        if ( $item.type -eq 'msi' ) {
+            $arguments = "/i `"$installer_filepath`""
+            if ( ! $item.interactive ) { $arguments += " /qn" }
+            if ( $item.args ) { $arguments += " $($item.args)" }
+            $installer_filepath = "msiexec"
+        }
+        # type: exe
+        else {
+            $arguments = $item.args
+        }
 
-            if ($item.args) {
-                $command += " $($item.args)"
-            }
-
-            $scriptblock = [scriptblock]::Create($command)
-
-            $LASTEXITCODE = 0
-            $success_exit_codes = $item.success_exit_codes
+        if ( $item.interactive ) {
+            warning " Interactive Install: follow prompts of the external installer" -noprefix
+        }
         
-            if (!$success_exit_codes) {
-                $success_exit_codes = 0
-            }
+        # execute
+        if ( $IS_VERBOSE ) { Write-Verbose "EXECUTING: `"$installer_filepath`" $arguments" }
+        $process = start $installer_filepath `
+            -ArgumentList $arguments `
+            -Wait `
+            -PassThru
 
-            $output = icm $scriptblock
-
-            if ($item.interactive) {
-                Write-Log -Level WARNING -Message "Starting interactive installer; when done, press any key to continue"
-                wait_anykey
-            }
-            else {
-                if ($LASTEXITCODE -notin ([string]$success_exit_codes).split(',')) {
-                    Write-Log -Level WARNING -Message $output
-                    throw "Command FAILED: '$command' (exit code: $LASTEXITCODE)"
-                }
-            }
-
-            continue
+        if ( $process.ExitCode -notin $success_exit_codes ) {
+            $executable = Split-Path $installer_filepath -Leaf
+            $failure = "`"$executable`" failed with exit code `"$($process.ExitCode)`""
         }
+    }
 
-        # chocolatey packages
-        if ($package_type.Key -eq 'chocolatey') {
-
-            $command = "& choco install `"$($item.name)`""
-
-            if ($item.args) {
-                $command += " --params `"$($item.args)`""
-            }
-
-            $command += " -y --force"
-            $scriptblock = [scriptblock]::Create($command)
-
-            $LASTEXITCODE = 0
-            $success_exit_codes = @(0, 1641, 3010)
-
-            $output = icm $scriptblock
-
-            if ($LASTEXITCODE -notin $success_exit_codes) {
-                Write-Log -Level WARNING -Message $output
-                throw "Command FAILED: '$command' (exit code: $LASTEXITCODE)"
-            }
-
-            continue
-        }
-
-        # powershell-get modules
-        if ($package_type.Key -eq 'powershellget') {
-            if (!(Get-Module $item.name)) {
-                if (!(Get-Module $item.name -ListAvailable)) {
-                    Install-Module $item.name -Force -Scope $item.scope
-                }
-                Import-Module $item.name -Force -DisableNameChecking
-            }
-
-            continue
-        }
+    if ( $failure ) {
+        error $failure
+        $index++
+    }
+    else {
+        $config.$stage.RemoveAt($index)
     }
 }
+
+# dump failed config
+if ( $config.$stage.Count -eq 0 ) {
+    $config.Remove($stage)
+    $excode = 0
+}
+
+if ( $UpdateConfigInPlace ) {
+    $config | ConvertTo-Yaml | Out-File $ConfigFilePath -Force -Encoding utf8
+}
+
+exit $excode
